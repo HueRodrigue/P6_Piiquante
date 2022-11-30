10bbd688eb5b2d2345a1f2f9654c0f521e8ba8bc
HueRodrigue <huerodrigue76@gmail.com>
2022-11-30 14:20:08 +0100

    authentification / post / delete / modify sauce

---
+++ backend/routes/sauce.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const auth = require('../middlewares/auth');
+const multer = require('../middlewares/multer-config');
+const sauceCtrl = require('../controllers/sauce');
+
+const router = express.Router();
+
+
+
+router.get('/',auth, sauceCtrl.getAllSauce);
+router.post('/',auth,multer, sauceCtrl.createSauce);
+router.get('/:id',auth, sauceCtrl.getOneSauce);
+router.put('/:id',auth,multer, sauceCtrl.modifySauce);
+router.delete('/:id',auth, sauceCtrl.deleteSauce);
+
+module.exports = router;
